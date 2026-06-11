6f88e757f7f7fe23047250c44cdd6fc3b114ab40
Pasindu Madushan <pasindu24hh@gmail.com>
2026-06-11 11:38:52 +0530

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1 className="text-3xl font-bold text-center mt-10">
+        QuickMarket
+      </h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
