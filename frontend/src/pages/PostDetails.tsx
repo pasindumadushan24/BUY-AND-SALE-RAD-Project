e6057947fababeeca13856dc546f65d6876a0153
Pasindu Madushan <pasindu24hh@gmail.com>
2026-06-22 23:42:08 +0530

    Changes

--- frontend/src/pages/PostDetails.tsx
+++ frontend/src/pages/PostDetails.tsx
@@ -92,6 +92,7 @@ function PostDetails() {
           ))}
         </div>
 
+
         {/* Details */}
         <div className="bg-white rounded-xl shadow-md p-5 mt-6">
           <h1 className="text-2xl md:text-3xl font-bold">
